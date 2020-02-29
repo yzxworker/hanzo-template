14be3b2b4b14470589b24a1b3c642a5902c3f4dd
yizhongxiang@xsyxsc.com <yizhongxiang@xsyxsc.com>
2020-02-29 14:39:01 +0800

    最基础模板

---
+++ demo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h-button type="default">默认按钮</h-button>
+    </div>
+</template>
+
+<script>
+import hButton from './index'
+export default {
+    name: "button-demo",
+    data() {
+        return {
+            type: 'primary'
+        }
+    },
+    components: {
+        hButton
+    },
+    methods: {
+    }
+}
+</script>
--- index.vue
+++ index.vue
@@ -1,57 +1,14 @@
 <template>
-    <button :class="classes( [ this.type, this.size, {
-                loading: this.loading,
-                disabled: this.disabled,
-                hairline: this.hairline,
-                block: this.block,
-                plain: this.plain,
-                round: this.round,
-                square: this.square
-            } ] )" @click="handleClick">
-        <span class="hanzo-button__text"><slot>{{text}}</slot></span>
-    </button>
+    <button>按钮</button>
 </template>
 
 <script>
-import { classesMixin } from '../mixins/classname';
-let classMixin = classesMixin('button');
 export default {
-    mixins: [classMixin],
-    name: "h-button",
-    props: {
-        text: String,
-        block: Boolean,
-        plain: Boolean,
-        round: Boolean,
-        square: Boolean,
-        loading: Boolean,
-        hairline: Boolean,
-        disabled: Boolean,
-        loadingText: String,
-        loadingType: String,
-        type: {
-            type: String,
-            default: 'default'
-        },
-        size: {
-            type: String,
-            default: 'normal'
-        },
-        loadingSize: {
-            type: String,
-            default: '20px'
-        }
-    },
+    name: 'h-button',
     methods: {
-        handleClick( e ) {
-            if ( !this.loading && !this.disabled ) {
-                this.$emit( 'click', e );
-            }
-        }
     }
 }
 </script>
 
 <style lang="less">
-@import './index.less';
 </style>
